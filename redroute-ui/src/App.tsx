869968b632ba1156f4485348d096abfb5a7a6643
ShlokM08 <shlok21421@iiitd.ac.in>
2025-08-24 20:08:29 +0300

    routing now fixed

--- redroute-ui/src/App.tsx
+++ redroute-ui/src/App.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "./SignIn";
 import RedRouteLandingUltra from "./RedRouteLandingUltra";
 
 export default function App() {
+  // Always start fresh: remove any previous "logged in" flag on first load
+  useEffect(() => {
+    localStorage.removeItem("rr_demo_user");
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -10,10 +16,10 @@ export default function App() {
         <Route index element={<SignIn />} />
         <Route path="/" element={<SignIn />} />
 
-        {/* Main site after you navigate from Sign-In */}
+        {/* Main site after you log in from Sign-In */}
         <Route path="/home" element={<RedRouteLandingUltra />} />
 
-        {/* Anything else -> send to Sign-In */}
+        {/* Anything else → send to Sign-In */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
