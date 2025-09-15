7dbee0b670cc2fcbe27d968e3843587f7dc4ceb4
ShlokM08 <shlok21421@iiitd.ac.in>
2025-09-15 18:19:36 +0300

    deploy debug 1

--- redroute-ui/src/EventDetail.tsx
+++ redroute-ui/src/EventDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Calendar, MapPin, Minus, Plus, CheckCircle2 } from "lucide-react";
 import { Button } from "./components/ui/button";
